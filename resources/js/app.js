c519661cb4cba91f80ae0f4f94f85eb2c8eaca77
Alexandre Unruh <alexandre@unruh.com.br>
2021-07-09 09:45:29 -0300

    Config files changed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+InertiaProgress.init()
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}`),
+  setup({ el, app, props, plugin }) {
+    createApp({ render: () => h(app, props) })
+      .use(plugin)
+      .mixin({ methods: {route: window.route} })
+      .mount(el)
+  },
+})
